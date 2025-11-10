3a91ffc43f41f319e8dc1f7e7b1daa0fc74820b0
Noya-Moshe <noya.moshe@post.runi.ac.il>
2025-11-10 18:47:33 +0200

    assignment completed

--- Cheers.java
+++ Cheers.java
@@ -9,5 +9,25 @@ Great work boaz!
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+            String name = args[0];
+            int num = Integer.parseInt(args[1]);
+            int length = name.length();
+            String vowels = "AEFHILMNORSX";
+
+            for(int i = 0; i < length; i++ ){
+                char letter = name.charAt(i);
+                int pos = vowels.indexOf(letter);
+
+                if(pos != -1){
+                        System.out.println("Give me an " + letter + ": "  + letter + "!");
+                } else{
+                        System.out.println("Give me a " + letter + ": " + letter + "!");
+                }
+                }
+            System.out.println("What does that spell?");
+            
+           for ( int i = 0; i < num; i++){
+                System.out.println( name.toUpperCase() + "!!!" );
+                }
         }
 }

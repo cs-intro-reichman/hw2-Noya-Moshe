f9a3f663969f7804a7ede7bfa7c8c2ad81867673
Noya-Moshe <noya.moshe@post.runi.ac.il>
2025-11-11 09:45:24 +0200

    assignment completed

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,19 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		double sum = 0.0; 
+        int sign = 1;
+		int denominator = 1;
+
+		for (int i = 0; i < num; i++){
+			sum = sum + sign*(1.0 / denominator);
+			denominator += 2;
+			sign *= -1;
+		}
+			System.out.println("pi according to Java: " + Math.PI);
+		    System.out.println("pi, approximated:     " + sum * 4);
+	
 	}
 }
+

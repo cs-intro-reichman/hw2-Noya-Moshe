59a3372cceac5cdf0a14312ddc44b7611a65d4ac
Noya-Moshe <noya.moshe@post.runi.ac.il>
2025-11-11 16:42:28 +0200

    assignment completed

--- Collatz.java
+++ Collatz.java
@@ -2,5 +2,38 @@
 public class Collatz {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
-	}
-}
+		int N = Integer.parseInt(args[0]);
+		String state = args[1];
+		
+		for(int seed = 1; seed <= N; seed++){
+		  int newNum = seed;
+		  int counter = 1;
+		  
+		  if(state.equals("v")){
+			System.out.print(newNum + " ");
+		  }
+		
+		  do { 
+			if (newNum % 2 == 0){
+				newNum = newNum / 2;
+		  } else {
+			newNum = (newNum * 3) +1;
+		  }
+		
+		counter++; 
+		
+
+		if(state.equals("v")){
+			System.out.print(newNum + " ");
+		}
+			
+		} while (newNum != 1);
+	
+
+		if (state.equals("v")){
+			System.out.println("(" + counter + ")");
+		}
+	} 
+	System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+   }
+ }
